731ecec23b30428de0cca7b6297fdd7e5b744a82
nithin-sudarsan <nithinsudarsan740@gmail.com>
2025-02-28 12:07:51 +0530

    Updated link and highlight under WorkExp section

--- src/components/WorkExp.svelte
+++ src/components/WorkExp.svelte
@@ -5,7 +5,7 @@
 <main>
     <span class="title2">How I pay my bills</span>
     <p>
-        As a software engineer at <a href="https://perfios.com" target="_blank" class = "highlight">Perfios</a>, where I've been working for a little over a year, I'm basically a financial detective with a keyboard. My team and I turn boring bank statements into goldmines of <span class="highlight tooltip">Insights<span class="tooltiptext">Fun fact: that's what our team is called, "Insights"</span></span>, helping banks make smarter lending decisions. <br><br>
+        As a software engineer at <a href="https://perfios.com" target="_blank" class = "highlight link">Perfios</a>, where I've been working for a little over a year, I'm basically a financial detective with a keyboard. My team and I turn boring bank statements into goldmines of <span class="highlight tooltip">Insights<span class="tooltiptext">Fun fact: that's what our team is called, "Insights"</span></span>, helping banks make smarter lending decisions. <br><br>
         Think of it as helping financial institutions swipe right or left on loan applications, but with more algorithms and fewer awkward matches. Sure, 'bank statement analysis' might not sound like a conversation starter at parties, but transforming complex data into powerful decision-making tools has its own kind of thrill – plus, those streaming subscriptions aren't going to pay for themselves!
     </p>
     <hr>
@@ -21,9 +21,13 @@
         text-decoration: none;
     }
     .highlight {
-        font-style:oblique;
+        font-style: oblique;
         font-weight: bold;
         transition: transform 0.4s ease;
+    }
+    .link {
+        text-decoration: underline;
+        font-style: normal;
     }
     .highlight:hover {
         cursor: pointer;
