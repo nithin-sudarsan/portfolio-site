a847d2b9d78c085f8d11406d7a32d6d542afcdfd
nithin-sudarsan <nithinsudarsan740@gmail.com>
2025-06-09 21:15:33 +0530

    added cv

---
+++ src/routes/cv/+page.svelte
@@ -0,0 +1,38 @@
+<main>
+    <a href="https://drive.google.com/uc?export=download&id=1B0cLFm7_Kl1uedWvev3KOtyVYjs8fwgf" download>
+        <p>Download a copy
+            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 22 22">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 13V4M7 14H5a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-4a1 1 0 0 0-1-1h-2m-1-5-4 5-4-5m9 8h.01"/>
+            </svg>
+        </p>
+    </a>
+    <iframe src="https://amaanalwvougcmmejbyf.supabase.co/storage/v1/object/public/cv//Nithin_Sudarsan_CV.pdf" id='cv'/>
+</main>
+<style>
+    main {
+        width: 100%;
+        height: 100vh;
+        overflow: hidden;
+    }
+
+    iframe {
+        width: 100%;
+        height: 100%;
+        border: none;
+    }
+    p {
+        text-align: right;
+        font-size: 1rem;
+        margin-top: 20px;
+        opacity: 0.4;
+        transition: 0.3s ease;
+    }
+    a {
+        text-decoration: none;
+        color: inherit;
+        cursor: pointer;
+    }
+    p:hover {
+        opacity: 0.7;
+    }
+</style>
