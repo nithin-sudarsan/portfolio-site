5af525cd32b61e4ee51398335e570f923db8bef8
nithin-sudarsan <nithinsudarsan740@gmail.com>
2025-06-20 17:32:48 +0530

    updated cv

--- src/routes/cv/+page.svelte
+++ src/routes/cv/+page.svelte
@@ -1,5 +1,5 @@
 <main>
-    <a href="https://drive.google.com/uc?export=download&id=1B0cLFm7_Kl1uedWvev3KOtyVYjs8fwgf" download>
+    <a href="https://drive.google.com/uc?export=download&id=1UJ1SzgN6U-vsSQ51-xwtAAJ8TF3NOlUP" download>
         <p>Download a copy
             <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 22 22">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 13V4M7 14H5a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-4a1 1 0 0 0-1-1h-2m-1-5-4 5-4-5m9 8h.01"/>
